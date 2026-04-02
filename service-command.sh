1b55ec0bf95bb57199c8fa8c38cc554766d8652e
Kirill Korablin <k@runtel.ru>
2026-04-02 13:15:13 +0300

    service-command.sh

---
+++ service-command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#основной https://github.com/sherbettt/Jenkins-cheats
+#запасные https://gitflic.ru/project/kkorablin/jenkins-cheats и https://gitverse.ru/sherbettt/Jenkins-cheats
+
+git pull;
+git push git@gitflic.ru:kkorablin/jenkins-cheats.git;
+git push git@gitverse.ru:sherbettt/Jenkins-cheats.git
